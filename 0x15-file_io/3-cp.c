43b295ee0f2b3ecbe37994e19bbfb281d0be9df9
SeipatiP <sayparty1@gmail.com>
2023-10-04 14:14:20 -0700

    program copies file content to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -54,7 +54,7 @@ int main(int argc, char *argv[])
 		nchars = read(file_from, buf, 1024);
 		if (nchars == -1)
 			error_file(-1, 0, argv);
-		nwr = write(file_to, buf, nchars);
+		nwr = write(file_to, buf, 1024);
 			if (nwr == -1)
 			error_file(0, -1, argv);
 	}
